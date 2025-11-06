eabfd7fdb1ca475b33b9e48dbb8e31397376aadc
Axel Waltersson <axelwaltersson@gmail>
2025-11-06 09:16:31 +0100

    Skapat en ny branch och lagt till Country-klubben som en sida.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 import start from './pages/start.js';
 import jazzClub from './pages/jazz-club.js';
 import metalClub from './pages/metal-club.js';
+import countryClub from './pages/country-club.js';
 
 // Our menu: label to display in menu and 
 // function to run on menu choice
 const menu = {
   "start": { label: 'Start', function: start },
   "jazz-klubben": { label: 'Jazz-klubben', function: jazzClub },
-  "metal-klubben": { label: 'Metal-klubben', function: metalClub }
+  "metal-klubben": { label: 'Metal-klubben', function: metalClub},
+  "country-klubben": {label: 'Country-klubben', function: countryClub}
 };
 
 function createMenu() {
---
+++ js/pages/country-club.js
@@ -0,0 +1,5 @@
+import clubInfoAndEvents from "../utils/club-info-and-events.js";
+
+export default async function countryClub() {
+  return clubInfoAndEvents('5cc6');
+}
