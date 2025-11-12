3e827e873da2898e53b78625b040a6b568bdfda2
Joel Lindström <test@fedora.com>
2025-11-12 21:49:57 +0100

    Förfinade html outline och gjorde om klubbskaparfunktion till att bli en event-skaparefunktion

--- js/pages/create-club.js
+++ js/pages/create-club.js
@@ -1,26 +1,19 @@
-export default async function createClub() { //eventNamn, eventBeskrivning, klubbID, eventDate
+export default async function createClub() { 
   return `
     <h2>Skapa event</h2>
-    <form>
-      <input name="event-name" placeholder="Eventnamn">
-      <input name="clubID" placeholder="KlubbID: XXXX">
-      <input name="event-price" placeholder="Kostnad">
-      <input type="date" placeholder="När är eventet?">
-
-      <textarea name="event-description" placeholder="Beskrivning av eventet"></textarea>
-      <input type="submit" value="Skapa nytt event"> 
-    </form>
-
-    <h2>Skapa en klubb</h2>
     <form id="create-club">
-      <input type="text" name="name" placeholder="Klubbnamn">
-      <textarea name="description" placeholder="Beskrivning"></textarea>
-      <input type="submit" value="Skapa">
+      <input name="eventName"   placeholder="Eventnamn">
+      <input name="clubID"      placeholder="KlubbID: XXXX">
+      <input name="eventDate"   placeholder="När är eventet?" type="date">
+
+      <textarea name="eventDescription" placeholder="Beskrivning av eventet"></textarea>
+
+      <input type="submit" value="Skapa"> 
     </form>
   `;
 }
 
-// Listar klubbar för html element (kanske inte används)
+// Listar klubbar för html element (får se om det används, hade varit bäst att visa klubbarna i en drop down meny)
 async function listClubs() {
   const clubsInDB = await (await fetch('http://localhost:3000/clubs', { method: 'GET' })).json();
   //const clubsData = await clubsInDB.json();
@@ -30,15 +23,16 @@ async function listClubs() {
 async function submitForm(event) {
   const target = event.target;
   // easy way to read values from forms - no querySelector needed!
-  const name = target.name.value;
-  const description = target.description.value;
-  await fetch('http://localhost:3000/clubs', {
+  const name = target.eventName.value;
+  const description = target.eventDescription.value;
+  const date = target.eventDate.value;
+  const clubID = target.clubID.value;
+  await fetch('http://localhost:3000/events', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name, description }) // stringify = serialize
+    body: JSON.stringify({ date, name, description, clubID }) // stringify = serialize
   });
-  // goto club-created page
-  location.hash = '#club-created';
+  location.hash = '#start';
 }
 
 // add event listener
