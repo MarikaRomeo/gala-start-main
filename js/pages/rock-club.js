ca04e52a0dd94fb6e39604f714126ee56a66496e
Joel Lindström <test@fedora.com>
2025-11-13 21:55:10 +0100

    lade till 'modern design' till hemsidan (avrundade hörn)

--- js/pages/rock-club.js
+++ js/pages/rock-club.js
@@ -16,7 +16,7 @@ async function clubEvents(clubId) {
       .toSorted((a, b) => a.date > b.date ? 1 : -1)
       .map(({ date, name, description }) => `
         <article class="event">
-          <h3>${name} <br> ${date}</h3>
+          <h2>${name} <br> ${date}</h2>
           <p>${description}</p>
           <button type="button" onclick="window.location.href='booking-form.html'">Skaffa biljett för '${name}'</button>
         </article>
@@ -39,40 +39,3 @@ document.addEventListener("DOMContentLoaded", async () => {
     element.innerHTML = `<p>Någonting gick fel!</p>`
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default async function countryClub() {
-//   const eventHtml = await clubEvents('5cc6');
-//   const countryClubHtml = await fetch('../country-club.html');
-//   console.log(countryClubHtml);
-//   //konverterar responsen från fetch till en string
-//   const countryClubHtmlString = await countryClubHtml.text();
-
-//   const parser = new DOMParser();
-//   //Konverterar countryClubHtmlString till ett dokument genom DOMparser
-//   const doc = parser.parseFromString(countryClubHtmlString, "text/html");
-//   console.log(doc);
-//   //Sparar innehållet från det elementet som använder klassen .eventSidebar i doc
-//   const eventContent = doc.querySelector('.eventContent');
-//   console.log(eventContent);
-  
-//   if(!eventContent){
-//     throw new Error ("eventContent could not load!")
-//   }
-//   eventContent.innerHTML = eventHtml;
-
-//   return doc.documentElement.outerHTML;
-// }
